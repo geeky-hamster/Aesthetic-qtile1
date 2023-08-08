1592f7238bd3e14b80c15d01982c3294b5cc378a
geeky-hamster <110613129+geeky-hamster@users.noreply.github.com>
2023-08-08 20:30:03 +0530

    up

--- install.sh
+++ install.sh
@@ -12,4 +12,5 @@ makepkg -si
 
 yay -Sy
 
-yay -S --needed - < s.txt
+yay -S blueberry bluez bluez-tools bluez-utils brave-bin alacritty alacritty-themes dmenu i3-wm i3blocks i3status hsetroot neofetch networkmanager noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra paccache-hook papirus-icon-theme pipewire pipewire-alsa  pipewire-pulse playerctl qtile qtile-extras rofi ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-hack ttf-jetbrains-mono ttf-jetbrains-mono-nerd windscribe-cli wireplumber neofetch htop fzf ncspot yay-bin archlinux-tweak-tool-dev-git xfce-polkit gvfs ksuperkey xorg-xsetroot xdg-user-dirs thunar thunar-volman thunar-archive-plugin
+
